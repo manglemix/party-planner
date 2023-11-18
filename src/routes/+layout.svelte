168175a521b804317110bd099a7f4d328dbd12e7
Najman <najman.husaini@utah.edu>
2023-11-18 10:27:32 -0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<div id="page">
+    <slot />
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@200;400;700&display=swap');
+
+    :global(*) {
+        font-family: 'Kanit', sans-serif;
+        line-height: 1;
+        margin: 0px;
+        padding: 0px;
+        color: white;
+    }
+
+    :global(body) {
+        display: flex;
+        flex-direction: column;
+        background-color:hsl(210, 60%, 3%);
+    }
+
+    #page {
+        position: fixed;
+        width: min(50rem, 100% - 3rem);
+        align-self: center;
+        align-items: center;
+        height: 100vh;
+        /* min-height: calc(100vh - 10rem); */
+        display: flex;
+        flex-direction: column;
+        background-color:hsl(210, 85%, 5%);
+        padding: 1rem 3rem 0rem 3rem;
+        box-shadow: 0px 0px 100px hsl(210, 30%, 32%);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import chatbg from '$lib/assets/chat-background.png';
+</script>
+
+<h1>Party Planner</h1>
+<h2>Create a complete plan, fit for any time, with just a simple conversation!</h2>
+
+<!-- <div id="chatbox-parent"> -->
+<section id="chatbox">
+    <img src={chatbg} alt="chat background">
+</section>
+<!-- </div> -->
+
+<style>
+    h1 {
+        text-align: center;
+        font-size: 8rem;
+    }
+
+    h2 {
+        font-weight: lighter;
+        margin-top: 1rem;
+        align-self: center;
+    }
+
+    /* #chatbox-parent {
+        height: 100%;
+    } */
+
+    #chatbox {
+        background-color: white;
+        /* position: absolute; */
+        /* top: 3rem; */
+        /* bottom: 3rem; */
+        width: min(35rem, 100% - 3rem);
+        margin-top: 3rem;
+        margin-bottom: 3rem;
+        height: 100%;
+    }
+
+    #chatbox img {
+        width: 100%;
+        object-fit: cover;
+        height: 100%;
+        filter: saturate(40%);
+    }
+</style>
